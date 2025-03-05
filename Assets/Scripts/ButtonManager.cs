4e4a3e1df13b8cdbf7bde338538004738defe39c
bollemedost <iluck23@student.aau.dk>
2025-03-05 14:10:13 +0100

    Main Menu:)

---
+++ Assets/Scripts/ButtonManager.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonManager : MonoBehaviour
+{
+    public GameObject difficultyPanel; // Assign DifficultyPanel in Inspector
+
+    public string easySceneName = "Aioli";
+    public string mediumSceneName = "Aioli";
+    public string hardSceneName = "Aioli";
+    public string tutorialSceneName = "Aioli";
+
+    public void Start()
+    {
+        difficultyPanel.SetActive(false); // Hide DifficultyPanel on Start
+    }
+
+    public void OnPlayButtonClick()
+    {
+        difficultyPanel.SetActive(true); // Show DifficultyPanel when Play button is clicked
+    }
+
+    public void OnEasyMode()
+    {
+        SceneManager.LoadScene(easySceneName); // Load EasyScene when Easy button is clicked
+    }
+
+    public void OnMediumMode()
+    {
+        SceneManager.LoadScene(mediumSceneName); // Load EasyScene when Easy button is clicked
+    }
+
+    public void OnHardMode()
+    {
+        SceneManager.LoadScene(hardSceneName); // Load EasyScene when Easy button is clicked
+    }
+
+    public void OnTutorialButtonClick()
+    {
+        SceneManager.LoadScene(tutorialSceneName); // Load EasyScene when Easy button is clicked
+    }
+}
