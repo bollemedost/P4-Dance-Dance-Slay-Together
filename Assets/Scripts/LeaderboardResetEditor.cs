480b814505a1ae39ed9024d4ae59ea508efde78c
MrTibor <rb68kx@student.aau.dk>
2025-04-10 10:56:38 +0200

    reset option

---
+++ Assets/Scripts/LeaderboardResetEditor.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+public class LeaderboardResetEditor : MonoBehaviour
+{
+    public enum LeaderboardType { Easy, Medium, Hard }
+
+    [SerializeField] private LeaderboardType leaderboardToReset;
+    [SerializeField] private bool resetNow = false;
+
+    void OnValidate()
+    {
+        if (resetNow)
+        {
+            switch (leaderboardToReset)
+            {
+                case LeaderboardType.Easy:
+                    PlayerPrefs.DeleteKey("EasyHighscores");
+                    Debug.Log(" Easy leaderboard reset.");
+                    break;
+                case LeaderboardType.Medium:
+                    PlayerPrefs.DeleteKey("MediumHighscores");
+                    Debug.Log(" Medium leaderboard reset.");
+                    break;
+                case LeaderboardType.Hard:
+                    PlayerPrefs.DeleteKey("AllHighscores");
+                    Debug.Log(" Hard leaderboard reset.");
+                    break;
+            }
+
+            PlayerPrefs.Save();
+            resetNow = false; // Uncheck automatically
+        }
+    }
+}
